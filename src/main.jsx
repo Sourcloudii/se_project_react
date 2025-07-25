8bc9592e22c47e449aa4bc105abb2a086cfb92e2
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-07-24 22:00:50 -0500

    switched to hashrouter instead of reactrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/se_project_react/">
+    <HashRouter basename="/se_project_react/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
