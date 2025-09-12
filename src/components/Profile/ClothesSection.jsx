3e866306d760573042b47af0125ae96098d2f548
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-09-12 03:21:57 -0500

    added filter for current user only

--- src/components/Profile/ClothesSection.jsx
+++ src/components/Profile/ClothesSection.jsx
@@ -1,17 +1,34 @@
 import "./ClothesSection.css";
 import ItemCard from "../ItemCard/ItemCard";
 
-function ClothesSection({ onCardClick, clothingItems, handleOpenAddModal }) {
+function ClothesSection({
+  onCardClick,
+  onCardLike,
+  clothingItems,
+  handleOpenAddModal,
+  currentUser,
+}) {
   return (
     <div className="clothes-section">
       <div className="clothes-section-group">
         <p className="clothes-section_text">Your items</p>
-        <button onClick={handleOpenAddModal} className="clothes-section-btn">+ Add New</button>
+        <button onClick={handleOpenAddModal} className="clothes-section-btn">
+          + Add New
+        </button>
       </div>
       <ul className="cards__list clothes-section_cards-list">
-        {clothingItems.map((item) => (
-          <ItemCard item={item} key={item._id} onCardClick={onCardClick} />
-        ))}
+        {currentUser?._id &&
+          clothingItems
+            .filter((item) => item.owner === currentUser._id)
+            .map((item) => (
+              <ItemCard
+                item={item}
+                key={item._id}
+                onCardClick={onCardClick}
+                onCardLike={onCardLike}
+                currentUser={currentUser}
+              />
+            ))}
       </ul>
     </div>
   );
