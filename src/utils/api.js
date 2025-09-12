9479602d6b544b6237e2f83286a1e73d86f54dfe
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-09-12 03:18:01 -0500

    added authorization & like/dislikeitem functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,28 +1,54 @@
-const baseUrl = "http://localhost:3001";
+const BASE_URL = "http://localhost:3001";
 
 export function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
 }
 
 export function getItems() {
-  return fetch(`${baseUrl}/items`).then(checkResponse);
+  return fetch(`${BASE_URL}/items`).then(checkResponse);
 }
 
-export function deleteClothingItem(Id) {
-  return fetch(`${baseUrl}/items/${Id}`, {
+export function deleteClothingItem(Id, token) {
+  return fetch(`${BASE_URL}/items/${Id}`, {
     method: "DELETE",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
   }).then(checkResponse);
 }
 
-export function postNewClothingItem({ name, imageUrl, weather }) {
-  return fetch(`${baseUrl}/items`, {
+export function postNewClothingItem({ name, imageUrl, weather }, token) {
+  return fetch(`${BASE_URL}/items`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
     body: JSON.stringify({
       name,
       imageUrl,
       weather,
     }),
+  }).then(checkResponse);
+}
+
+export function likeItem(itemId, token) {
+  return fetch(`${BASE_URL}/items/${itemId}/likes`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+}
+
+export function dislikeItem(itemId, token) {
+  return fetch(`${BASE_URL}/items/${itemId}/likes`, {
+    method: "Delete",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
   }).then(checkResponse);
 }
